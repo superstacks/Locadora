7b552b15ed9371248852c1aea7d402353fc68352
KleitonRufino <kleitonhd@hotmail.com>
2014-11-21 15:22:34 -0300

    fsd

---
+++ src/com/wordpress/programandojava/utils/createTables.java
@@ -0,0 +1,19 @@
+package com.wordpress.programandojava.utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class createTables {
+
+	/**
+	 * @param args
+	 */
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("locadora");
+		System.out.println("olaaaa");
+	}
+
+}
